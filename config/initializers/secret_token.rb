0526bc35921c9726b556156a3592174b1ee86e39
Evan Machnic <emachnic@gmail.com>
2010-08-27 23:21:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Broadmac::Application.config.secret_token = '67b659d7b68df12993cb2d29b630ce3cc963d4564c329cd6f3e8d08579fc125e952c5a9a3316e7ad00f447771ab56219449cb987a96c667e1652ba6243f67b83'
